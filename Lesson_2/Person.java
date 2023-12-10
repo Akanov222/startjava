8ebd4cef8b24c009596b76cf0d27cd1e4fa6d3e7
Akanov222 <146616561+Akanov222@users.noreply.github.com>
2023-12-10 15:47:08 +0300

    Create Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    int age = 21;
+    double height = 1.85;
+    double weight = 91.5;
+    String name = "Josh";
+    String gender = "Male";
+    
+    void move() {
+        System.out.println(name + " move");
+    }
+    void run() {
+        System.out.println(name + " run");
+    }
+    void sit() {
+        System.out.println("sit");
+    }
+    void talk() {
+        System.out.println("talk");
+    }
+    void studyJava() {
+        System.out.println("study Java");
+    }
+}
